a5ae11abe5ae3c473653bc82fd04b817a741e3dd
Diogo-Serra <di-code@outlook.com>
2026-05-05 21:43:08 +0100

    echo_validator resolved

--- exam_03/easy/echo_validator.py
+++ exam_03/easy/echo_validator.py
@@ -1,6 +1,11 @@
 # Echo validator
 
 def echo_validator(text: str) -> bool:
+    if not text:
+        return False
+    for x in text:
+        if x.isdigit():
+            return False
     text = text.replace(' ', '').upper()
     j = len(text) - 1
     for i, x in enumerate(text):
@@ -11,4 +16,18 @@ def echo_validator(text: str) -> bool:
 
 
 if __name__ == "__main__":
+
     print(echo_validator("racecar"))
+#    True
+    print(echo_validator("A man a plan a canal Panama"))
+#    True
+    print(echo_validator("race a car"))
+#    False
+    print(echo_validator("Was it a car or a cat I saw"))
+#    True
+    print(echo_validator("hello"))
+#    False
+    print(echo_validator("Madam Im Adam"))
+#    True
+    print(echo_validator(""))
+#    False
