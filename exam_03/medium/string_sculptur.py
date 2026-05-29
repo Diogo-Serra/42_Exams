e388dd13f4a0dec5b5aab07bce50d2b876b36991
Diogo-Serra <di-code@outlook.com>
2026-05-29 22:20:54 +0100

    updates

---
+++ exam_03/medium/string_sculptur.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+def string_sculptor(text: str) -> str:
+    stack = []
+    counter = "lower"
+    for i, ch in enumerate(text, start=1):
+        if ch.isalpha():
+            if i == 1:
+                stack.append(ch.lower())
+            elif counter == "lower":
+                stack.append(ch.upper())
+                counter = "upper"
+            elif counter == "upper":
+                stack.append(ch.lower())
+                counter = "lower"
+        else:
+            stack.append(ch)
+    return ''.join(stack)
+
+
+print(string_sculptor("hello"))
+print(string_sculptor("Hello World"))
+print(string_sculptor("aBc123def"))
+print(string_sculptor("Python3.9!"))
+print(string_sculptor(""))
