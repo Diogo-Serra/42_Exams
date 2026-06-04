4fa778daa88d62337a0f78fec05f068dd9e71104
Diogo-Serra <di-code@outlook.com>
2026-06-04 16:05:37 +0100

    updates

--- exam_03/medium/bracket_validator.py
+++ exam_03/medium/bracket_validator.py
@@ -1,14 +1,7 @@
+# Bracket Validator
+
 def bracket_validator(s: str) -> bool:
     stack = []
-    pairs = {')': '(', ']': '[', '}': '{'}
-    opens = set(pairs.values())
 
-    for ch in s:
-        if ch in opens:
-            stack.append(ch)
-        elif ch in pairs:
-            if not stack or stack[-1] != pairs[ch]:
-                return False
-            stack.pop()
 
-    return len(stack) == 0
+print(bracket_validator('(){}[]'))
