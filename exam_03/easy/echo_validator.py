c747f8fa4fad207ad612576b6c67c6a08ec46dbc
Diogo-Serra <di-code@outlook.com>
2026-06-04 10:17:03 +0100

    updates

--- exam_03/easy/echo_validator.py
+++ exam_03/easy/echo_validator.py
@@ -2,5 +2,5 @@
 
 def echo_validator(text: str) -> bool:
     _text = text.replace(' ', '').upper()
-    _cpy = text.replace(' ', '').upper()[::-1]
-    return _text == _cpy
+    _reversed = text.replace(' ', '').upper()[::-1]
+    return _text == _reversed
