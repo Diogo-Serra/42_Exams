dac66c99415d1a564acb3884e5026569eeef0b07
Diogo-Serra <di-code@outlook.com>
2026-06-10 15:00:32 +0100

    updates

--- exam_03/medium/bracket_validator.py
+++ exam_03/medium/bracket_validator.py
@@ -2,6 +2,14 @@
 
 def bracket_validator(s: str) -> bool:
     stack = []
+    valid = {')': '(', '}': '{', ']': '['}
+    start = valid.values()
+    for ch in s:
+        if ch in start:
+            stack.append(ch)
+        elif stack[-1] and stack[-1] == valid[ch]:
+            stack.pop()
+    return len(stack) == 0
 
 
 print(bracket_validator('(){}[]'))
