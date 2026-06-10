ad93b1e59f09e7144ee382de48ae746bb8d71030
Diogo-Serra <di-code@outlook.com>
2026-06-10 15:17:12 +0100

    updates

---
+++ exam_03/medium/pattern_tracker.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def pattern_tracker(text: str) -> int:
+    count = 0
+    for i, ch in enumerate(text):
+        if i == len(text) - 1:
+            return count
+        if ch.isdigit():
+            digit1 = int(ch)
+            if text[i + 1].isdigit() and int(text[i + 1]) == digit1 + 1:
+                count += 1
+    return count
+
+
+print(pattern_tracker('01234567'))
